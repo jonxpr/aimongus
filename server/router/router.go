a2e4a103d98af8659847dcafb888dce84a47a3e0
jonxpr <jon.xpr@gmail.com>
2023-12-22 16:00:08 +0000

    created router

---
+++ server/router/router.go
@@ -0,0 +1,37 @@
+package router
+
+import (
+	"server/internal/user"
+	"server/internal/ws"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+var r *gin.Engine
+
+func InitRouter(wsHandler *ws.Handler) {
+	r = gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "http://localhost:3000"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
+	r.POST("/ws/createRoom", wsHandler.CreateRoom)
+	r.GET("/ws/joinRoom/:roomId", wsHandler.JoinRoom)
+	r.GET("/ws/getRooms", wsHandler.GetRooms)
+	r.GET("/ws/getClients/:roomId", wsHandler.GetClients)
+}
+
+func Start(addr string) error {
+	return r.Run(addr)
+}
