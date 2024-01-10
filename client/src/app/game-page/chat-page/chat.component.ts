4274910317b6be9188899666b5164e7c9cd5c8d4
zitongcharliedeng <zitongcharliedeng2003@gmail.com>
2024-01-10 16:01:47 +0000

    Add chat to vote page timed redirect

--- client/src/app/game-page/chat-page/chat.component.ts
+++ client/src/app/game-page/chat-page/chat.component.ts
@@ -1,39 +1,49 @@
 import { Component } from '@angular/core';
 import { GameServerService } from '../../game-server.service';
-type State = "Chat" | "Vote"
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+
+type State = "Chat" | "Vote";
+
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.sass'
+  styleUrls: ['./chat.component.sass']
 })
 export class ChatComponent {
-  messageToSend : string = ""
-  incomingMessages: any[] = []
-  state: State = "Chat"
+  messageToSend: string = "";
+  incomingMessages: any[] = [];
+  state: State = "Chat";
 
-  constructor(private gameServer:GameServerService){}
+  constructor(private gameServer: GameServerService, private router: Router, private route: ActivatedRoute) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.gameServer.receiveMessageFromServer()?.subscribe((message) => {
-      if (message.type === "Message"){
-        message.content = message.content.replace(/"/g, '')
+      if (message.type === "Message") {
+        message.content = message.content.replace(/"/g, '');
         this.incomingMessages.push(message);
       }
-    })
+    });
+
+    // do init at here for current route.
+
+    setTimeout(() => {
+      console.log("WHATTATATAAT", this.route.parent)
+      this.router.navigate(['./vote'], { relativeTo: this.route.parent });
+    }, 60000); // 60s Chatting Phase of Game
   }
 
-  sendMessage(): void{
+  sendMessage(): void {
     console.log(this.messageToSend);
     this.gameServer.sendMessageToServer(this.messageToSend);
     this.messageToSend = "";
   }
 
-  changeState():void{
-    if (this.state === "Chat"){
-      this.state = "Vote"
-    }else{
-      this.state = "Chat"
+  changeState(): void {
+    if (this.state === "Chat") {
+      this.state = "Vote";
+    } else {
+      this.state = "Chat";
     }
   }
-
 }
