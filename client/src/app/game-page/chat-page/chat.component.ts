71aebf8f9b57e0d166589779309dfd5028acd41c
zitongcharliedeng <zitongcharliedeng2003@gmail.com>
2024-01-10 16:43:53 +0000

    Add timer to chat too

--- client/src/app/game-page/chat-page/chat.component.ts
+++ client/src/app/game-page/chat-page/chat.component.ts
@@ -15,22 +15,55 @@ export class ChatComponent {
   incomingMessages: any[] = [];
   state: State = "Chat";
 
-  constructor(private gameServer: GameServerService, private router: Router, private route: ActivatedRoute) {}
+  constructor(private gameServer: GameServerService, private router: Router, private route: ActivatedRoute) {
+    this.timer(1);
+  }
 
   ngOnInit() {
-    this.gameServer.receiveMessageFromServer()?.subscribe((message) => {
-      if (message.type === "Message") {
-        message.content = message.content.replace(/"/g, '');
-        this.incomingMessages.push(message);
-      }
-    });
-
-    // TIMER 
+    // FEAT: Timed phase redirect
 
     setTimeout(() => {
-      this.router.navigate(['./vote'], { relativeTo: this.route.parent });
-    }, 60000); // 60s Chatting Phase of Game
+      this.router.navigate(['./reveal'], { relativeTo: this.route.parent });
+    }, 60000); // 60s Voting Phase of Game
   }
+
+  // FEAT: Timer display
+  timerValue: any; // Initialize with a default value
+  timerValueProportion: any; // Initialize with a default value
+  
+  timer(minute: number) {
+    let seconds: number = minute * 60;
+    let textSec: any = "0";
+    let statSec: number = 60;
+  
+    const prefix = minute < 10 ? "0" : "";
+  
+    const updateTimerValues = () => {
+      this.timerValue = `${prefix}${Math.floor(seconds / 60)}:${textSec}`;
+      this.timerValueProportion = seconds/(minute*60);
+    };
+  
+    updateTimerValues(); // Initial update
+  
+    const timer = setInterval(() => {
+      seconds--;
+  
+      if (statSec != 0) statSec--;
+      else statSec = 59;
+  
+      if (statSec < 10) {
+        textSec = "0" + statSec;
+      } else textSec = statSec;
+  
+      updateTimerValues(); // Update values on each interval
+  
+      if (seconds == 0) {
+        console.log("finished");
+        clearInterval(timer);
+      }
+    }, 1000);
+  }
+  // END FEAT: Timer display
 
   sendMessage(): void {
     console.log(this.messageToSend);
